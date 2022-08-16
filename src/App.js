b246846db66be3580bb99924d8a60fbfc72a01ef
Muktar Sadiq <muktarsadiq.m@yahoo.com>
2022-08-16 16:47:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import CryptoDetail from "./pages/CryptoDetail";
+import CryptoHome from "./pages/CryptoHome";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<CryptoHome />} />
+        <Route path="/coin/:id" element={<CryptoDetail />} />
+      </Routes>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
